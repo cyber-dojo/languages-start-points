d382fbc729c4c5b12b44916c27dfc8fefb0a0bfe
Jon Jagger <jon@jaggersoft.com>
2022-08-21 09:59:02 +0100

    [ci skip] Make readme Github Actions button a link

--- .circleci/merkely_log_deployment.sh
+++ .circleci/merkely_log_deployment.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -Eeu
 
-readonly MY_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 readonly MERKELY_ENVIRONMENT="${1}"
 readonly MERKELY_HOST="${2}"
 readonly MERKELY_CHANGE=merkely/change:latest
