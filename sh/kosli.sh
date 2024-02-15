232ed00676400f3dd7f020e23736d47e1714e80d
JonJagger <jon@kosli.com>
2024-02-15 10:24:26 +0000

    CI: add .snyk file as attachment in kosli-attest-snyk

--- sh/kosli.sh
+++ sh/kosli.sh
@@ -65,6 +65,7 @@ kosli_attest_snyk()
     --artifact-type=docker \
     --host="${hostname}" \
     --api-token="${api_token}" \
+    --attachments="$(repo_root)/.snyk" \
     --name=languages-start-points.snyk-scan \
     --scan-results="$(repo_root)/snyk.json"
 }
