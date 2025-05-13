7ef2907f3bcaa92699be6ed229eeb8a6ccaa945a
JonJagger <jon@kosli.com>
2025-05-13 12:27:16 +0100

    Move to new commander image

--- sh/lib.sh
+++ sh/lib.sh
@@ -14,12 +14,10 @@ git_commit_tag()
 
 image_name()
 {
-  # set in
-  # export $(echo_versioner_env_vars)
   echo "${CYBER_DOJO_LANGUAGES_START_POINTS_IMAGE}"
 }
 
-image_sha()
+image_base_sha()
 {
-  docker run --rm $(image_name) sh -c 'echo ${SHA}'
+  docker run --rm $(image_name) sh -c 'echo ${CYBER_DOJO_START_POINTS_BASE_SHA}'
 }
